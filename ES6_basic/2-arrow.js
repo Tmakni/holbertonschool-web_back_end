f1ad68eca3460c309305ca492061da48fbf428c6
tom <tom.makni@gmail.com>
2025-03-25 11:19:03 +0100

    ok

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,10 @@
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
-    this.addNeighborhood = function (newNeighborhood) {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
+    const self = this;
+    this.addNeighborhood = (newNeighborhood) => {
+      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return self.sanFranciscoNeighborhoods;
     };
   }
+  
