bed3008b012aa032d340e322c5f8bbc0cb7c8295
tom <tom.makni@gmail.com>
2025-03-25 13:37:50 +0100

    ok

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,7 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+    return initialNumber + expansion1989 + expansion2019;
+  }  
+   {
     if (expansion1989 === undefined) {
       expansion1989 = 89;
     }
@@ -6,5 +9,6 @@ export default function getSumOfHoods(initialNumber, expansion1989, expansion201
     if (expansion2019 === undefined) {
       expansion2019 = 19;
     }
+
     return initialNumber + expansion1989 + expansion2019;
   }
