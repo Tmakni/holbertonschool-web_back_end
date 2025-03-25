e77bedcb182e3cb51f5c76d3c9b32dbf64e0d479
tom <tom.makni@gmail.com>
2025-03-25 13:47:10 +0100

    ok

---
+++ ES6_basic/.js
@@ -0,0 +1,10 @@
+
+/mnt/c/Users/tomma/Desktop/Dossier/Python/holbertonschool-web_back_end/ES6_basic/1-block-scoped.js
+  6:11  error  'task' is assigned a value but never used   no-unused-vars
+  7:11  error  'task2' is assigned a value but never used  no-unused-vars
+
+/mnt/c/Users/tomma/Desktop/Dossier/Python/holbertonschool-web_back_end/ES6_basic/3-default-parameter.js
+  13:3  error  Parsing error: 'return' outside of function
+
+✖ 3 problems (3 errors, 0 warnings)
+
