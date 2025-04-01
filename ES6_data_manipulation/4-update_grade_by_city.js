781f51ecafe968a012964310a92e01d4cf12b9ab
tom <tom.makni@gmail.com>
2025-04-01 15:05:50 +0200

    ok

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    if (!Array.isArray(students) || !Array.isArray(newGrades) || typeof city !== 'string') {
+      return [];
+    }
+  
+    return students
+      .filter(student => student.location === city)
+      .map(student => {
+        const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+        return {
+          ...student,
+          grade: gradeObj ? gradeObj.grade : 'N/A'
+        };
+      });
+  }
+  
