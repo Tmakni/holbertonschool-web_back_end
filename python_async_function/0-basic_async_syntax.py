ba6f1f3a1e2667c91e76a76cd8b6933223051fe1
Tmakni <tom.makni@gmail.com>
2025-07-29 14:08:51 +0200

    ok

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,9 @@ commentaire
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    commentaire
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
