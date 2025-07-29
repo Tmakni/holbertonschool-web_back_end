1713b255c768481c833a1294123dfeb9a36568f0
Tmakni <tom.makni@gmail.com>
2025-07-29 14:30:25 +0200

    ok

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import List
+"""
+commentaire
+"""
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    commentaire
+    """
+    delays = []
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
