3ef911811cb9b443e10f8b23baeefbecae1886de
Tmakni <tom.makni@gmail.com>
2025-07-29 11:42:59 +0200

    pycode

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from typing import list
 
+
 def sum_mixed_list(mxd_list: list[int, float]) -> float:
     return sum(mxd_list)
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
     return (k, float(v**2))
