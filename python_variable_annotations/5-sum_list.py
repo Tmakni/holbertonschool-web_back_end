36ab51028cb7779f45e5c9b839385299c2a409bd
Tmakni <tom.makni@gmail.com>
2025-07-29 11:44:58 +0200

    pycode

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/ python3
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     return sum(input_list)
