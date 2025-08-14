2f7743712d334d7ab50316e8ce14b4cbf125e161
Tom Makni <tom.makni@gmail.com>
2025-08-14 12:32:02 +0200

    ok

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+"""
+list all
+"""
+
 
 def list_all(mongo_collection):
+    """
+    list all
+    """
     if mongo_collection is None:
         return []
     return list(mongo_collection.find())
