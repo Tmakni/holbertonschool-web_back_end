7bfc18ed23c17a3c3d72e98777f3dc808ad94193
Tom Makni <tom.makni@gmail.com>
2025-08-21 11:32:40 +0200

    ok

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -6,10 +6,7 @@ export default class HolbertonCourse {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    if (!students.every((s) => typeof s === 'string')) {
+    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
 
@@ -21,7 +18,6 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
-
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
@@ -32,7 +28,6 @@ export default class HolbertonCourse {
   get length() {
     return this._length;
   }
-
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
@@ -43,7 +38,6 @@ export default class HolbertonCourse {
   get students() {
     return this._students;
   }
-
   set students(newStudents) {
     if (!Array.isArray(newStudents) || !newStudents.every((s) => typeof s === 'string')) {
       throw new TypeError('Students must be an array of strings');
