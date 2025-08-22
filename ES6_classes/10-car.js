f0999479d0626898870d0a3e76bc0de3bc11c1f7
Tom Makni <tom.makni@gmail.com>
2025-08-22 12:05:40 +0200

    ok

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,19 +1,16 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        if(typeof brand !== 'string') {
-            throw new TypeError('brand must be a string'); 
-        }
-        if(typeof motor !== 'string') {
-            throw new TypeError('motor must be a string');
-        }
-        if(typeof color !== 'string') {
-            throw new TypeError('color must be a string');
-        }
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
-    cloneCar() {
-        return new Car(this.brand, this.motor, this.color)
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Ctor = this.constructor[Symbol.species];
+    return new Ctor();
+  }
 }
