3c36d96b08d11e5ebc8efb04684048aaf188823a
Tom Makni <tom.makni@gmail.com>
2025-08-26 09:52:09 +0200

    ok

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-export default function getStudentsByLocation() {
+export default function getStudentsByLocation(list, city) {
   if (!Array.isArray(list)) return [];
   return list.filter((student) => student.location === city);
 }
