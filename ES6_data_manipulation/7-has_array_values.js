7e24b3223428a42c8dee699cf19f6d7be4e719bb
Tom Makni <tom.makni@gmail.com>
2025-08-26 11:40:39 +0200

    ok

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,8 @@
+export default function hasValuesFromArray(s, arr) {
+    if (!(s instanceof Set) || !Array.isArray(arr)) return false;
+    for (const value of arr) {
+        if (!s.has(value)) return false; {
+    }
+            return true;
+        }
+}
