7c3d93ab1167e3ba3c04ced58df2b98ae07bee41
Tom Makni <tom.makni@gmail.com>
2025-08-26 15:01:18 +0200

    ES6_promise: 0-promise + divers

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,17 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number' || Number.isNaN(length)) {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() { return this._name; }
@@ -24,7 +15,7 @@ export default class HolbertonCourse {
 
   get length() { return this._length; }
   set length(value) {
-    if (typeof value !== 'number' || Number.isNaN(value)) {
+    if (typeof value !== 'number' || !Number.isFinite(value)) {
       throw new TypeError('Length must be a number');
     }
     this._length = value;
