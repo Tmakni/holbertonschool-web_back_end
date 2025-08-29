c1dee3cc3567eef36c6023204e3eaf1b1257050d
Tom Makni <tom.makni@gmail.com>
2025-08-29 10:56:36 +0200

    ok

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,15 +2,15 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const tasks = [
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName),
-    ];
+  const tasks = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
 
   return Promise.allSettled(tasks).then((results) =>
     results.map((r) => ({
       status: r.status,
-      value: r.status === 'fulfilled' ? r.value : r.reason,
+      value: r.status === 'fulfilled' ? r.value : String(r.reason),
     }))
   );
 }
