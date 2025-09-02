3a4acbacd1c73e3b2944fa570b4737796cfcf104
Tom Makni <tom.makni@gmail.com>
2025-09-02 14:08:58 +0200

    ok

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,17 @@
-console.log("Welcome to Holberton School, what is your name?")
-process.stdin.setEncoding('utf-8');
+const readline = require('readline');
 
-process.stdin.on('data', (chunk) => {
-  const name = chunk.split('\n')[0].trim();
-  if (name.length > 0) {
-    console.log(`Your name is: ${name}`);
-  }
+console.log('Welcome to Holberton School, what is your name?');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+rl.on('line', (line) => {
+  console.log(`Your name is: ${line}`);
+  rl.close();
 });
 
-process.on('SIGINT', () => {
+rl.on('close', () => {
   console.log('This important software is now closing');
-  process.exit(0);
 });
