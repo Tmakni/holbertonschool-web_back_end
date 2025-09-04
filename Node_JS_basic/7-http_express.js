5befdee1df68029fa9fcbdc5bb282ab1dd6a6025
Tom Makni <tom.makni@gmail.com>
2025-09-04 11:51:58 +0200

    ok

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const contStudents = require('3-read_file_async.js')
+
+const app = express();
+const databaseFile = process.argv[2]
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!')
+});
+
+app.get('/students', async(req, res) => {
+    res.type('text/plain');
+    let reponse = 'This is the list of our students'
+    try {
+      const result = await contStudents(databaseFile)
+      reponse += result;
+      res.send(reponse)
+    } catch (err) {
+        reponse += err.message;
+        res.send(reponse);
+    }
+})
+
+app.listen(1245, () => {
+
+});
+module.exports = app;
