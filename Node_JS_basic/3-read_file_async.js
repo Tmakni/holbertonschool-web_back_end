bc15051ffad60569b5f091d00e6d5c9425ae44cc
Tom Makni <tom.makni@gmail.com>
2025-09-04 12:02:44 +0200

    ok

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,16 +1,21 @@
 const fs = require('fs').promises;
+
 async function countStudents(path) {
   try {
     const data = await fs.readFile(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
+
     const studentsByField = {};
     let totalStudents = 0;
+
     for (const line of lines) {
       const fields = line.split(',');
       const firstName = fields[0].trim();
       const field = fields[3].trim();
+
       if (!studentsByField[field]) studentsByField[field] = [];
       studentsByField[field].push(firstName);
+
       totalStudents += 1;
     }
     console.log(`Number of students: ${totalStudents}`);
@@ -19,9 +24,11 @@ async function countStudents(path) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
       output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
     }
+
     return output.trim();
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
