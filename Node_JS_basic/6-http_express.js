a8f401e420169e098a48f7acd5a608f481674124
Tom Makni <tom.makni@gmail.com>
2025-09-04 12:16:37 +0200

    ok

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,9 +1,10 @@
+// 6-http_express.js
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+app.get('/', (_req, res) => {
+  res.send('Hello Holberton School!');
 });
 
 app.listen(1245);
